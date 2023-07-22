82eb0e4ed89178b4111b8820160f9a360e8ad6d9
tianxin-personal <96083098+tianxin-personal@users.noreply.github.com>
2023-07-22 12:37:59 -0500

    start from the scratch

--- littlelemon/tests/test_views.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-from restaurant.models import Menu
-
-class MenuViewTest(TestCase):
-    def setUp(self):
-        Menu.objects.create(title="IceCream",price=80,inventory=100)
--- restaurant/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Menu,Booking
-# Register your models here.
-my_models = [Menu,Booking] # iterable list for multiple models
-
-admin.site.register(my_models)
